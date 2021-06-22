23741714871fd66252ddd5357524748c0419ebed
tobasanmi <tobasanmi@gmail.com>
2021-06-22 09:31:07 +0100

    edited the search placeholder

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
     let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
     let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     let day = days[d.getDay()]
+    console.log('>>>',d);
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
@@ -40,7 +41,7 @@ function App() {
           <input 
           type = 'text'
           className= 'search-bar'
-          placeholder = 'search...'
+          placeholder = 'Enter a country/city'
           onChange = {(e) => setQuery(e.target.value)}
           value = {query}
           onKeyPress={search}
